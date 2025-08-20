427a9fec3fb9b74eea5071fd5a8b5b9a89dedb17
dhinesh-KM <dhineshmuthu2002@gmail.com>
2025-08-20 22:36:49 +0530

    ngnix added

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 // Determine the environment and load the appropriate .env file
 const env = process.env.NODE_ENV || 'dev'
-config({ path: `.env.${env}`})
+// config({ path: `.env.${env}`})
 
 export const port = process.env.PORT
 export const mongoURI = process.env.MONGO_URI_LOCAL
