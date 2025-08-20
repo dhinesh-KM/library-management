2acbaacc47d51c7f5202b2ac4bb55ba1f8a427f8
dhinesh-KM <dhineshmuthu2002@gmail.com>
2025-08-20 21:30:25 +0530

    update schema and validation

--- src/validation/schema.js
+++ src/validation/schema.js
@@ -1,14 +1,14 @@
-import Joi from 'joi';
-import moment from 'moment';
+import Joi from 'joi'
+import moment from 'moment'
 
 const date_validator = (value, helpers) => {
-    const date = moment(value, "DD-MM-YYYY");
+    const date = moment(value, "DD-MM-YYYY")
 
     if (!date.isValid())
-        return helpers.message('published date must be in the format DD-MM-YYYY');
+        return helpers.message('published date must be in the format DD-MM-YYYY')
 
     if ( date.isSameOrAfter(moment(), 'day'))
-        return helpers.message("published date should be a past date");
+        return helpers.message("published date should be a past date")
 
     return value
 }
@@ -19,7 +19,7 @@ export const registerSchema = Joi.object(
         email : Joi.string().email().messages({'string.email': 'Invalid email address.'}),
         password : Joi.string().min(6).required(),
     }
-).options({ stripUnknown: true });
+).options({ stripUnknown: true })
 
 export const bookCreateSchema = Joi.object(
     {
@@ -29,9 +29,9 @@ export const bookCreateSchema = Joi.object(
         // publicationDate: Joi.date().required(),
         publicationDate: Joi.string().required().custom(date_validator, 'custom date validation'),
         genre: Joi.string().trim().required(),
-        copies: Joi.number().integer().min(0).required(),
+        copies: Joi.number().integer().greater(0).required()
     }
-).options({ stripUnknown: true });
+).options({ stripUnknown: true })
 
 export const bookUpdateSchema = Joi.object(
     {
@@ -40,7 +40,7 @@ export const bookUpdateSchema = Joi.object(
         isbn: Joi.string().trim(),  
         publicationDate: Joi.string().custom(date_validator, 'custom date validation'),
         genre: Joi.string().trim(),
-        copies: Joi.number().integer().min(0),
+        copies: Joi.number().integer().greater(0),
     }
 ).min(1) // require at least one field to be provided
- .options({ stripUnknown: true });
+ .options({ stripUnknown: true })
--- src/validation/validator.js
+++ src/validation/validator.js
@@ -1,22 +1,21 @@
 
-import {CustomError} from '../utility/customerror.js';
+import {CustomError} from '../utility/customerror.js'
 
 function result(req,res,next,value,error)
 {
     if (error)
         //console.log( "---",error.message)
         throw new CustomError(error.message.replace(/"/g, '') , 400)
-    req.body = value;
-    next();
+    req.body = value
+    next()
 
 }
 
 export const validate_payload =  ( schema ) => {
     return (req, res, next) => {
-        console.log("000000")
-        const {value,error} = schema.validate(req.body, {abortEarly : false,  stripUnknown: true });
+        const {value,error} = schema.validate(req.body, {stripUnknown: true })
 
         //console.log("v:",value,"\ne:",error)
-        return result(req,res,next,value,error);
+        return result(req,res,next,value,error)
     }
 }
