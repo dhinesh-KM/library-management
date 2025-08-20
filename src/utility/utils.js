c3e1f4f4b302a3a006d872d52148fe138d03ff77
dhinesh-KM <dhineshmuthu2002@gmail.com>
2025-08-20 21:29:36 +0530

    utils added

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,2 +1,11 @@
 export const isPositiveInt = (v) =>
-  Number.isInteger(Number(v)) && Number(v) > 0;
+  Number.isInteger(Number(v)) && Number(v) > 0
+
+export const successResponse = (res, data = null, message = null, statusCode = 200) => {
+  const response = { error: false }
+
+  if (message) response.msg = message
+  if (data) response.data = data
+
+  return res.status(statusCode).json(response)
+}
